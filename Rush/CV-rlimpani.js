0e2c77c367e34b0003bd44b53b965cf211d65348
iamtrtle <67051299@kmitl.ac.th>
2026-09-18 14:38:36 +0700

    finish

---
+++ Rush/CV-rlimpani.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const textArray = [
+        "Data Engineer", 
+        "Computer Vision Dev", 
+        "Gamer & BRIIZE"
+    ];
+    
+    const typingDelay = 100; 
+    const erasingDelay = 50; 
+    const newTextDelay = 2000; 
+    let textArrayIndex = 0;
+    let charIndex = 0;
+    
+    const typedTextSpan = document.getElementById("typewriter");
+
+    function type() {
+        if (charIndex < textArray[textArrayIndex].length) {
+            typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, typingDelay);
+        } 
+        else {
+            setTimeout(erase, newTextDelay);
+        }
+    }
+
+    function erase() {
+        if (charIndex > 0) {
+            typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(erase, erasingDelay);
+        } 
+        else {
+            textArrayIndex++;
+            if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+            setTimeout(type, typingDelay + 1100);
+        }
+    }
+
+    if (textArray.length) {
+        setTimeout(type, 1000);
+    }
+});
